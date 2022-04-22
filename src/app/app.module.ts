85a092c99d21a464094fd3d0db2609a9287ce373
juris-hub <ivan.1jurisic@gmail.com>
2022-04-22 16:26:12 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shell/header/header.component';
+import { LanguageSelectorComponent } from './shell/language-selector/language-selector.component';
+import { SidebarComponent } from './shell/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    LanguageSelectorComponent
   ],
   imports: [
     BrowserModule,
