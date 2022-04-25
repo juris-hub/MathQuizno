280eaa023e2c641c3d4e9d2d75d524cb137bc0c0
juris-hub <ivan.1jurisic@gmail.com>
2022-04-25 23:34:50 +0200

    Trying to transfer data from header to sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { HeaderComponent } from './shell/header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,15 @@ export class AppComponent implements OnInit {
 
   constructor(private primengConfig: PrimeNGConfig){}
 
+  sidebarStatus!: any;
+
   ngOnInit(): void {
       this.primengConfig.ripple = true;
+  }
+
+  onHeaderClick($event:any){
+    console.log($event);
+    this.sidebarStatus = $event
+    console.log('this is my status...' + this.sidebarStatus)
   }
 }
